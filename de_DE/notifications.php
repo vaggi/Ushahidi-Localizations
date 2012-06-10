fd3720c601b296e0025fee750b8336b02241c091
Florian Wagner <flow.public@vaggi.org>
2012-06-10 06:03:17 +0300

    Update de_DE/notifications.php

--- de_DE/notifications.php
+++ de_DE/notifications.php
@@ -17,5 +17,14 @@
 	'admin_new_sms' => array(
 		'message' => 'Eine neue Textnachricht wurde an Deine Website geschickt.',
 		'subject' => 'Neue Textnachricht',
+	),		
+	'member_new_alert' => array(
+		'message' => 'Du hast eine neue Meldung erhalten',
+		'subject' => 'Neue Meldung!'
+	),
+	'member_new_message' => array(
+		'message' => 'Du hast eine private Nachricht erhalten',
+		'subject' => 'Neue private Nachricht',
+		'footer' => 'Um zu antworten gehe bitte zu: '	
 	));
 ?>
