934e441dfd9bbfa9f749b483bc3aea847df57461
vaggi <flow@vaggi.org>
2012-06-10 05:28:04 +0300

    Update German Translation datetime.php

--- de_DE/datetime.php
+++ de_DE/datetime.php
@@ -29,5 +29,53 @@
 	'wednesday' => array(
 		'abbv' => 'MI',
 		'full' => 'Mittwoch',
-	));
+	),
+    'january' => array(
+        'abbv' => 'Jan',
+        'full' => 'Januar',
+    ),
+    'february' => array(
+        'abbv' => 'Feb',
+        'full' => 'Februar',
+    ),
+    'march' => array(
+        'abbv' => 'Mär',
+        'full' => 'März',
+    ),
+    'april' => array(
+        'abbv' => 'Apr',
+        'full' => 'April',
+    ),
+    'may' => array(
+        'abbv' => 'Mai',
+        'full' => 'Mai',
+    ),
+    'june' => array(
+        'abbv' => 'Jun',
+        'full' => 'Juni',
+    ),
+    'july' => array(
+        'abbv' => 'Jul',
+        'full' => 'Juli',
+    ),
+    'august' => array(
+        'abbv' => 'Aug',
+        'full' => 'August',
+    ),
+    'september' => array(
+        'abbv' => 'Sep',
+        'full' => 'September',
+    ),
+    'october' => array(
+        'abbv' => 'Okt',
+        'full' => 'Oktober',
+    ),
+    'november' => array(
+        'abbv' => 'Nov',
+        'full' => 'November',
+    ),
+    'december' => array(
+        'abbv' => 'Dez',
+        'full' => 'Dezember',
+    ));
 ?>
