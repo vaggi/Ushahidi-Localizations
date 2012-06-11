8e69e30c36f38b020c259587153979de2b25100d
Florian Wagner <flow.public@vaggi.org>
2012-06-11 04:31:01 +0300

    updated de_DE/bug.php

--- de_DE/bug.php
+++ de_DE/bug.php
@@ -5,19 +5,19 @@
 		'required' => 'Bitte geben Sie den Sicherheitscode ein.',
 	),
 	'email' => array(
-		'email' => 'Das eMailfeld scheint keine gültige eMailadresse zu enthalten.',
-		'length' => 'Das eMailfeld muss mindestens 4 und höchstens 64 Zeichen lang sein.',
-		'required' => 'Das eMailfeld ist erforderlich, wenn das Kästchen angehakt wurde.',
+		'email' => 'Das Emailfeld scheint keine gültige eMailadresse zu enthalten.',
+		'length' => 'Das Emailfeld muss mindestens 4 und höchstens 64 Zeichen lang sein.',
+		'required' => 'Das Emailfeld muss ausgefüllt sein, wenn das Kästchen angehakt wurde.',
 	),
 	'error' => array(
-		'required' => 'Das Fehlerfeld ist notwendig.',
+		'required' => 'Das Fehlerfeld muss ausgefüllt sein.',
 	),
 	'subject' => array(
 		'length' => 'Das Betreffeld muss mindestens 3 Zeichen lang sein.',
-		'required' => 'Das Betreff-Feld ist notwendig.',
+		'required' => 'Das Betreff-Feld muss ausgefüllt sein.',
 	),
 	'yourname' => array(
 		'length' => 'Das Namensfeld muss mindestens 3 Zeichen lang sein.',
-		'required' => 'Das Namensfeld ist notwendig.',
+		'required' => 'Das Namensfeld muss ausgefüllt sein.',
 	));
 ?>
