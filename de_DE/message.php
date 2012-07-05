3cee1e0d8cc3a88c16ca8d17d75a2c6302680004
Florian Wagner <flow.public@vaggi.org>
2012-07-05 20:00:58 +0300

    Update master

--- de_DE/message.php
+++ de_DE/message.php
@@ -7,16 +7,16 @@
 	'email' => array(
 		'email' => 'Das eMail-Feld scheint keine gültige Mailadresse zu enthalten.',
 		'length' => 'Das eMail-Feld muss zwischen 4 und 64 Zeichen lang sein.',
-		'required' => 'Das eMail-Feld ist erforderlich, wenn die Box angehakt wurde. ',
+		'required' => 'Das eMail-Feld muss ausgefüllt sein, wenn die Box angehakt wurde. ',
 	),
 	'message' => array(
-		'required' => 'Das Kommentarfeld ist notwendig.',
+		'required' => 'Das Kommentarfeld muss ausgefüllt sein.',
 	),
 	'name' => array(
 		'length' => 'Das Namensfeld muss min. 3 Zeichen lang sein.',
-		'required' => 'Das Namensfeld ist notwendig.',
+		'required' => 'Das Namensfeld muss ausgefüllt sein.',
 	),
 	'phone' => array(
-		'length' => 'Das Telefonfeld ist nicht gültig.',
+		'length' => 'Das Telefonfeld enthält einen ungültigen Wert.',
 	));
 ?>
