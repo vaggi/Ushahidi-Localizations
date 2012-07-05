dbadbc689126b4f73991528d945fdb221f0b9a7e
Florian Wagner <flow.public@vaggi.org>
2012-07-05 20:31:44 +0300

    Update master

--- de_DE/settings.php
+++ de_DE/settings.php
@@ -1,42 +1,42 @@
 <?php
 	$lang = array(
 	'allow_comments' => array(
-		'between' => 'The allow comments field does not appear to contain a valid value?',
-		'required' => 'The allow comments field is required.',
+		'between' => 'Das Kommentare-erlauben-Feld scheint einen ungültigen Wert zu enthalten?',
+		'required' => 'Das Kommentare-erlauben-Feld muss ausgefüllt sein.',
 	),
 	'allow_feed' => array(
-		'between' => 'The include feed field does not appear to contain a valid value?',
-		'required' => 'The include feed field is required.',
+		'between' => 'Das Feed-einbinden-Feld scheint einen ungültigen Wert zu enthalten?',
+		'required' => 'The Feed-einbinden-Feld muss ausgefüllt sein.',
 	),
 	'allow_reports' => array(
-		'between' => 'The allow reports field does not appear to contain a valid value?',
-		'required' => 'The allow reports field is required.',
+		'between' => 'Das Berichte-erlauben-Feld scheint einen ungültigen Wert zu enthalten?',
+		'required' => 'Das Berichte-erlauben-Feld muss ausgefüllt sein.',
 	),
 	'allow_stat_sharing' => array(
-		'between' => 'The stat sharing field does not appear to contain a valid value?',
-		'required' => 'The stat sharing field is required.',
+		'between' => 'Das stat-sharing-Feld scheint einen ungültigen Wert zu enthalten?',
+		'required' => 'Das stat-sharing-Feld muss ausgefüllt sein.',
 	),
 	'api' => array(
-		'default_record_limit' => 'Standardanzahl von Records der API-Anforderung',
-		'maximum_record_limit' => 'Maximale Anzahl an Records die pro API-Anfrage geholt werden.',
-		'maximum_requests_per_ip_address' => 'Maximale Anzahl an API-Anfragen durch pro IP-Adresse',
+		'default_record_limit' => 'Standardanzahl von Einträgen der API-Anforderung',
+		'maximum_record_limit' => 'Maximale Anzahl an Einträgen die pro API-Anfrage geholt werden.',
+		'maximum_requests_per_ip_address' => 'Maximale Anzahl an API-Anfragen pro IP-Adresse',
 	),
 	'api_akismet' => array(
-		'alpha_numeric' => 'The Akismet field does not appear to contain a valid value?',
-		'length' => 'The Akismet field does not appear to contain a valid value?',
+		'alpha_numeric' => 'Das Akismet-Feld scheint einen ungültigen Wert zu enthalten?',
+		'length' => 'Das Akismet-Feld scheint einen ungültigen Wert zu enthalten?',
 	),
 	'banner_image' => array(
-		'default' => 'Something went wrong with your banner image upload.',
-		'size' => 'The size of your banner exceeds the size limit for this upload.',	
+		'default' => 'Es ist ein Fehler aufgetreten beim Upload deines Bannerbildes.',
+		'size' => 'Die Größe deines Banners übertrifft das Größenlimit für diesen Upload.',	
 	),
 	'api_default_record_limit' => 'Standard API Grenze',
 	'cache_pages' => array(
-		'between' => 'Das Cache Feld ist notwendig.',
-		'required' => 'Das Cache Feld ist ungültig',
+		'between' => 'Das Cache-Feld muss ausgefüllt sein.',
+		'required' => 'Das Cache-Feld enthält einen ungültigen Wert',
 	),
 	'cache_pages_lifetime' => array(
-		'in_array' => 'Das Cache Feld ist ungültig',
-		'required' => 'Das Cache Feld ist notwendig.',
+		'in_array' => 'Das Cache-Feld enthält einen ungültigen Wert',
+		'required' => 'Das Cache-Feld muss ausgefüllt sein.',
 	),
 	'cleanurl' => array(
 		'clean_url_disabled' => 'It looks like your server is not configured to handle clean URLs. You will need to change the configuration of your server before you can enable clean URLs. See more info on how to enable clean URLs at this forum <a href="http://forums.ushahidi.com/topic/server-configuration-for-apache-mod-rewrite" target="_blank">post</a>',
@@ -46,15 +46,15 @@
 	),
 	'clickatell_api' => array(
 		'length' => 'The Clickatell API number field must be no more 20 characters long.',
-		'required' => 'The Clickatell API number field is required.',
+		'required' => 'The Clickatell API number field muss ausgefüllt sein.',
 	),
 	'clickatell_password' => array(
 		'length' => 'The Clickatell Password field must be at least 5 and no more 50 characters long.',
-		'required' => 'The Clickatell Password field is required.',
+		'required' => 'The Clickatell Password field muss ausgefüllt sein.',
 	),
 	'clickatell_username' => array(
 		'length' => 'The Clickatell Username field must be no more 50 characters long.',
-		'required' => 'The Clickatell Username field is required.',
+		'required' => 'The Clickatell Username field muss ausgefüllt sein.',
 	),
 	'configure_map' => 'Konfiguriere Karte',
 	'default_location' => 'Standardposition',
@@ -72,7 +72,7 @@
 	),
 	'email_password' => array(
 		'length' => 'The Mail Server Password field must be at least 5 and no more 50 characters long.',
-		'required' => 'The Mail Server Password field is required.',
+		'required' => 'The Mail Server Password field muss ausgefüllt sein.',
 	),
 	'email_port' => array(
 		'length' => 'The Mail server port field is too long',
@@ -80,11 +80,11 @@
 	),
 	'email_servertype' => array(
 		'length' => 'The Mail server port field is too long',
-		'required' => 'The Mail Server Type field is required.',
+		'required' => 'The Mail Server Type field muss ausgefüllt sein.',
 	),
 	'email_username' => array(
 		'length' => 'The Mail Server Username field must be no more 50 characters long.',
-		'required' => 'The Mail Server Username field is required.',
+		'required' => 'The Mail Server Username field muss ausgefüllt sein.',
 	),
 	'facebook' => array(
 		'title' => 'Facebook Setup Options',
@@ -103,11 +103,11 @@
 	),
 	'items_per_page' => array(
 		'between' => 'The items per page (Frontend) field does not appear to contain a valid value?',
-		'required' => 'The items per page (Frontend) field is required.',
+		'required' => 'The items per page (Frontend) field muss ausgefüllt sein.',
 	),
 	'items_per_page_admin' => array(
 		'between' => 'The items per page (Admin) field does not appear to contain a valid value?',
-		'required' => 'The items per page (Admin) field is required.',
+		'required' => 'The items per page (Admin) field muss ausgefüllt sein.',
 	),
 	'map' => array(
 		'default_location' => 'Setting up your map provider is a straight- forward process. Select a provider, obtain an API key from the provider\'s site, and enter the API key',
@@ -161,11 +161,11 @@
 	),
 	'site_name' => array(
 		'length' => 'The site name field must be at least 3 and no more 50 characters long.',
-		'required' => 'The site name field is required.',
+		'required' => 'The site name field muss ausgefüllt sein.',
 	),
 	'site_tagline' => array(
 		'length' => 'The tagline field must be at least 3 and no more 100 characters long.',
-		'required' => 'The tagline field is required.',
+		'required' => 'The tagline field muss ausgefüllt sein.',
 	),
 	'sms' => array(
 		'clickatell_api' => 'Ihre Clickatell API Nummer',
